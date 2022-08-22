17f74fe2f0700c553c426a987e43b2e47282b311
unknown <victorio5054@gmail.com>
2022-08-22 15:53:33 -0300

    estilização

--- src/views/Empresas/Empresas.jsx
+++ src/views/Empresas/Empresas.jsx
@@ -14,8 +14,9 @@ function Empresas() {
         <div className='containerPaginaTopo'>
             <section className='containerPrimeiroTexto'>
                 <h3>O futuro da saúde no seu bolso</h3>
-                <h1>Retenha e atraia os melhores talentos com um ecossistema de saúde em forma de benefício pago por uso.</h1>
-                <p>Uma rede credenciada de médicos, dentistas, psícologos e muito mais.<br></br>Disponíveis para sua equipe, quando, onde e como ela quiser. Tudo em uma assinatura que cabe no seu orçamento.</p>
+                <h1 className='tituloContainerPrimeiroTexto'>Retenha e atraia os melhores talentos com um ecossistema de saúde em forma de benefício pago por uso.</h1>
+                <p>Uma rede credenciada de médicos, dentistas, psícologos e muito mais.
+                Disponíveis para sua equipe, quando, onde e como ela quiser. Tudo em uma assinatura que cabe no seu orçamento.</p>
                 <div className='containerForm'>
                     <FormEnvio
                     classNameLabel='labelFormEnvio'
